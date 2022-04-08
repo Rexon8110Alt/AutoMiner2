b600433c3bb93ebc9e0f26fd0c10559b43e8ad3b
Rexon8110Alt <102179306+Rexon8110Alt@users.noreply.github.com>
2022-04-08 17:49:32 +0200

    Update core.java

--- src/core.java
+++ src/core.java
@@ -1,7 +1,5 @@
 import java.awt.*;
-
 import static java.awt.event.InputEvent.BUTTON1_DOWN_MASK;
-
 import static java.lang.Thread.sleep;
 
 public class core {
@@ -15,24 +13,30 @@ public class core {
         robot.mouseMove(650,435);
         robot.mousePress(BUTTON1_DOWN_MASK);
         */
+
         robot.mouseMove(550, 435);
 
 
         for (int x = 0; x < 3; x++) {
-            for (int i = 0; i < 6; i++) {
-
+            for (int y = 0; y < 5; y++) {
                 robot.mousePress(BUTTON1_DOWN_MASK);
                 robot.mouseMove(655, 435);
                 robot.mouseRelease(BUTTON1_DOWN_MASK);
                 sleep(3000);
 
+                robot.mouseMove(565, 500);
+                robot.mousePress(BUTTON1_DOWN_MASK);
+                robot.mouseRelease(BUTTON1_DOWN_MASK);
+                sleep(480);
                 robot.mouseMove(550, 435);
 
-
             }
-            robot.mouseMove(655, 435);
-            robot.mousePress(BUTTON1_DOWN_MASK);
-            robot.mouseRelease(BUTTON1_DOWN_MASK);
+
+                robot.mouseMove(655, 435);
+                robot.mousePress(BUTTON1_DOWN_MASK);
+                sleep(1000);
+                robot.mouseRelease(BUTTON1_DOWN_MASK);
         }
     }
 }
+
